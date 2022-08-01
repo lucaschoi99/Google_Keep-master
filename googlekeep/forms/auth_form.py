7343dcdc819bdf9044c33e7fe04afa7a393767d9
Minsoo Choi <73485743+lucaschoi99@users.noreply.github.com>
2022-08-01 18:18:34 +0900

    Update auth_form.py

--- googlekeep/forms/auth_form.py
+++ googlekeep/forms/auth_form.py
@@ -1,16 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
 
-
 class LoginForm(FlaskForm):
-    user_id = StringField('User Id', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-
+  user_id = StringField('User Id', validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
 
 class RegisterForm(LoginForm):
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo(
-        'repassword', message='Passwords must match.')])
-    repassword = PasswordField('Confirm Password', validators=[DataRequired()])
-
-    user_name = StringField('User Name', validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired(), EqualTo('repassword', message='Passwords must match.')])
+  repassword = PasswordField('Confirm Password', validators=[DataRequired()])
+  user_name = StringField('User Name', validators=[DataRequired()])
